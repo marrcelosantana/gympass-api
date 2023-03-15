bfca09210f314ea643711bbd17b6ecdcb08dfdfb
marrcelosantana <marcelosantana@alu.ufc.br>
2023-03-15 15:58:49 -0300

    [Gympass-Api] - Init Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from "fastify";
 
+import { PrismaClient } from "@prisma/client";
+
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Marcelo Santana",
+    email: "marcelo@email.com.br",
+  },
+});
