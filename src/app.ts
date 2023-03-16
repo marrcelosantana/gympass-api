928b9aa3bb16032b88e174823d23223838a437a2
marrcelosantana <marcelosantana@alu.ufc.br>
2023-03-16 15:38:26 -0300

    [Gympass-Api] - Bug fix

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,9 @@ export const app = fastify();
 
 const prisma = new PrismaClient();
 
-prisma.user.create({
-  data: {
-    name: "Marcelo Santana",
-    email: "marcelo@email.com.br",
-  },
-});
+// prisma.user.create({
+//   data: {
+//     name: "Marcelo Santana",
+//     email: "marcelo@email.com.br",
+//   },
+// });
