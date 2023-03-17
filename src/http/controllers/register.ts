568153312457caf7ead02387c73d345dcecbe4d2
marrcelosantana <marcelosantana@alu.ufc.br>
2023-03-17 18:18:29 -0300

    [Gympass-Api] - Use cases

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { hash } from "bcryptjs";
-import { prisma } from "@/lib/prisma";
 import { z } from "zod";
+import { registerUseCase } from "@/use-cases/register";
 
 export async function register(
   request: FastifyRequest,
@@ -15,25 +14,11 @@ export async function register(
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const password_hash = await hash(password, 6);
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (userWithSameEmail) {
-    return response.status(409).send("Email já existente!");
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (error) {
+    return response.status(409).send();
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  });
 
   return response.status(201).send();
 }
