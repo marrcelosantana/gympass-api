e5801d467e31af01be7d1cc00364b0fba5629bae
marrcelosantana <marcelosantana@alu.ufc.br>
2023-03-21 19:43:38 -0300

    [Gympass-Api] - Add factory pattern

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import { UserAlreadyExistsError } from "./../../use-cases/errors/user-already-exists-error";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
-import { RegisterUseCase } from "@/use-cases/register";
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case";
 
 export async function register(
   request: FastifyRequest,
@@ -17,8 +16,7 @@ export async function register(
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository();
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+    const registerUseCase = makeRegisterUseCase();
 
     await registerUseCase.execute({ name, email, password });
   } catch (error) {
