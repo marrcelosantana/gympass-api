47c0d1ada91711b1335d628cc98ab7182977aa62
marrcelosantana <marcelosantana@alu.ufc.br>
2023-03-22 22:03:22 -0300

    [Gympass-Api] - Check-ins validation

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -17,6 +17,15 @@ export class CheckInUseCase {
     userId,
     gymId,
   }: CheckInUseCaseParams): Promise<CheckInUseCaseResponse> {
+    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date()
+    );
+
+    if (checkInOnSameDay) {
+      throw new Error();
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
