0a7d6eedf56bebfa3052af312a6a33b7d49ac7fc
marrcelosantana <marcelosantana@alu.ufc.br>
2023-04-02 21:37:56 -0300

    [Gympass-Api] - Add search gyms use cases and tests

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -5,16 +5,6 @@ import { GymsRepository } from "../gyms-repository";
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = [];
 
-  async findById(id: string) {
-    const gym = this.items.find((item) => item.id === id);
-
-    if (!gym) {
-      return null;
-    }
-
-    return gym;
-  }
-
   async create(data: Prisma.GymCreateInput) {
     const gym = {
       id: data.id ?? randomUUID(),
@@ -27,6 +17,16 @@ export class InMemoryGymsRepository implements GymsRepository {
     };
 
     this.items.push(gym);
+
+    return gym;
+  }
+
+  async findById(id: string) {
+    const gym = this.items.find((item) => item.id === id);
+
+    if (!gym) {
+      return null;
+    }
 
     return gym;
   }
